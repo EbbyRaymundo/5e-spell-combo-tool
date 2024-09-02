ea49e3cf466532289c9c3003fc93aa0bd633b60d
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-01 22:56:55 -0400

    All Gestalt tables made and working

--- Populate_Gestalt.py
+++ Populate_Gestalt.py
@@ -73,22 +73,41 @@ def create_fusion_table(connection):
 	Initialize the Fusion table with the PK constraint. This table will be
 	populated later.
 	'''
+	connection.execute(
+		'''
+		CREATE TABLE Fusion(
+			fusion_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			effect TEXT UNIQUE NOT NULL
+		)
+		'''
+	)
 
 def create_spell_fusion_table(connection):
 	'''
 	Initialize the Spell_Fusion junction table with FK constraints. This
 	table will be populated later when the Fusion tables are added.
 	'''
-
+	connection.execute(
+		'''
+		CREATE TABLE Spell_Fusion(
+			spell_id INTEGER NOT NULL,
+			fusion_id INTEGER NOT NULL,
+			FOREIGN KEY (spell_id) REFERENCES Spell(spell_id),
+			FOREIGN KEY (fusion_id) REFERENCES Fusion(fusion_id)
+		)
+		'''
+	)
 
 def main():
 
 	with sqlite3.connect("Gestalt.db") as connection:
 		spell_table, class_table, spell_class_table = import_spell.format_spell_csv("spell_data/all_5e_spells.csv")
 
-		create_spell_table(connection, spell_table)
-		create_class_table(connection, class_table)
-		create_spell_class_table(connection, spell_class_table)
+		#create_spell_table(connection, spell_table)
+		#create_class_table(connection, class_table)
+		#create_spell_class_table(connection, spell_class_table)
+		#create_fusion_table(connection)
+		#create_spell_fusion_table(connection)
 
 	return 0
 
