f896d9273d15f50c6e8541039a886b9589c19282
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-03 00:26:44 -0400

    Changed addXYZ and addLink to properly insert necessary values only

--- Update_Gestalt.py
+++ Update_Gestalt.py
@@ -5,34 +5,50 @@ TODO: potentially use Pandas to get values into dataframes
 before inserting them into tables.
 '''
 
-def add_XYZ(XYZ: list):
+def add_XYZ(XYZ: list, classes: list = None):
 	'''
 	Add a list of XYZ spells into the Spell table and associate each XYZ
-	spell with every character class.
-
-	TODO: Add a method to specify which classes have access to ED spells if
-	it isn't all character classes.
+	spell with every character class unless provided a list to specify.
 	'''
 
 	with sqlite3.connect("Gestalt.db") as connection:
 		gestalt_cursor = connection.cursor()
 		# XYZ have no spell school, do not use concentration, can't be rituals, and have no upcast effects
-		gestalt_cursor.executemany("INSERT INTO Spell VALUES (?, ?, ?, ?, ?, ?, ?, NULL, 0, 0, 'XYZ', NULL)", list)
+		gestalt_cursor.executemany(
+			"""
+			INSERT INTO Spell(spell_name, level, casting_time, duration, range, components, description, concentration, ritual, spell_type) 
+			VALUES (?, ?, ?, ?, ?, ?, ?, 0, 0, 'XYZ')
+			""",
+			list
+			)
 
 	return
 
 
 def add_Link(Link: list):
-
+	"""
+	Add a list of XYZ spells into the Spell table and associate each XYZ
+	spell with every character class unless provided a list to specify.
+	"""
 	with sqlite3.connect("Gestalt.db") as connection:
 		gestalt_cursor = connection.cursor()
 		# Links have no spell school, do not use concentration, can't be rituals, and have no upcast effects
-		gestalt_cursor.executemany("INSERT INTO Spell VALUES (?, ?, ?, ?, ?, ?, ?, ?, NULL, 0, 'Link', NULL)", list)
+		gestalt_cursor.executemany(
+			"""
+			INSERT INTO Spell(spell_name, level, casting_time, duration, range, components, description, concentration, ritual, spell_type) 
+			VALUES (?, ?, ?, ?, ?, ?, ?, 0, 0, 'Link')
+			""",
+			list
+			)
 
 	return
 
 
 def add_Fusion(Fusion: list):
+	"""
+	Add a list of Fusion effects to the Fusion table and their associated
+	spells to the Spell_Fusion table.
+	"""
 
 	return
 
