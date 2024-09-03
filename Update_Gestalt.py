5453e212ca839e7b19e88ef26e476c3a44f0957d
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-02 23:45:24 -0400

    Add method to insert list of XYZ into the Spell table
    Need to add to the method to associate the XYZ with all spellcaster classes too.

--- Update_Gestalt.py
+++ Update_Gestalt.py
@@ -1,11 +1,22 @@
 import sqlite3
 
-
+'''
+TODO: potentially use Pandas to get values into dataframes
+before inserting them into tables.
+'''
 
 def add_XYZ(XYZ: list):
+	'''
+	Add a list of XYZ spells into the Spell table and associate each XYZ
+	spell with every character class.
+
+	TODO: Add a method to specify which classes have access to an XYZ if
+	it isn't all character classes.
+	'''
 
 	with sqlite3.connect("Gestalt.db") as connection:
 		gestalt_cursor = connection.cursor()
+		gestalt_cursor.executemany("INSERT INTO Spell VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", list)
 
 	return
 
