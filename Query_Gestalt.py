19bc15cc5d1383c816919d93c8863ee337783543
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-04 23:55:44 -0400

    Added stubs and documentation for Fusion, Synchro querying

---
+++ Query_Gestalt.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+
+def find_Accel_Synchro_target():
+	"""
+	Prompt the user to find enter a spell name, then prompt the user to select
+	an accel synchro route, then query Gestalt.db for available spells.
+	"""
+
+
+
+	return
+
+def find_Fusion_target():
+
+	return
