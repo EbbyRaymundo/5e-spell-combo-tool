bce3fbc27f782332f5996d276bfe68573cf37e55
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-05 12:09:55 -0400

    Add error handling for accel synchro modes

--- src/Query_Gestalt.py
+++ src/Query_Gestalt.py
@@ -9,11 +9,19 @@ def find_Accel_Synchro_target(source_spell: str, synchro_route: str):
 
 	Raises
 	------
-	No spell found with the provided spell name.
-		source_spell was not found in the database.
-	Multiple spells found with the provided spell name.
-		source_spell had multiple matches in the database.
+	ValueError
+		synchro_route not a valid Accel Synchro route (counterspelled, reaction, main action)
+	ValueError
+		source_spell not found in the database.
+	ValueError
+		source_spell has multiple matches in the database.
+
+	TODO: add 3 different queries to handle the different synchro routes
 	"""
+	synchro_route = synchro_route.lower().strip()
+
+	if not synchro_route in ["counterspelled", "reaction", "main action"]:
+		raise ValueError(f"{synchro_route} not a valid Accel Synchro route (counterspelled, reaction, main action)")
 
 	with sqlite3.connect("../Gestalt.db") as connection:
 		gestalt_cursor = connection.cursor()
@@ -21,12 +29,13 @@ def find_Accel_Synchro_target(source_spell: str, synchro_route: str):
 
 		check_provided_spell = gestalt_cursor.fetchall()
 
-		# 
+		# important that our query finds 1 and only 1 result
 		if not check_provided_spell:
-			raise Exception("No spell found with the provided spell name.")
+			raise ValueError("source_spell not found in the database.")
 		
 		elif len(check_provided_spell) > 1:
-			raise Exception(f"Multiple spells found with the provided spell name: {[spell[1] for spell in check_provided_spell]}.")
+			raise ValueError(f"Multiple spells found with the provided spell name: {[spell[1] for spell in check_provided_spell]}.")
+
 
 		return gestalt_cursor.fetchall()
 
