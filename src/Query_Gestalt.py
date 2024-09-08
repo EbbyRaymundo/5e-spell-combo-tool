9a94ae0058d062a9bb03e1f042d272b1a611e0b4
EbbyRaymundo <ebraymundo24@gmail.com>
2024-09-08 10:11:21 -0400

    Broke Accel Synchro getter into 3 functions
    Wisened up and split the Accel Synchro getter into 3 different functions so that a "synchro_route" mode parameter was no longer necessary.

--- src/Query_Gestalt.py
+++ src/Query_Gestalt.py
@@ -1,34 +1,30 @@
 import sqlite3
 
 
-
-def find_Accel_Synchro_target(source_spell: str, synchro_route: int):
+# TODO: decide if I want to add error handling to the Accel Synchro getters to
+#		check if the input spell has a duration greater than Instantaneous.
+def get_counterspelled_Accel_Synchro(source_spell: str):
 	"""
-	Given a spell and Accel Synchro route specifier (counterspelled: 0,
-	reaction: 1, main action: 2), determine the available spell options.
-	Refer to README for details on different Accel Synchro routes.
+	Given a spell with a duration greater than instantaneous, determine the
+	eligible Reaction spells to Accel Synchro into.
+
+	Parameters
+	----------
+	source_spell: str
+		Completely or partially matching unique spell name within the database.
 
 	Returns
 	-------
 	target_spells: list[tuple]
-		List of tuples representing valid Accel Synchro target spells
+		List of tuples of Reaction Accel Synchro target spells.
 
 	Raises
 	------
 	ValueError
-		synchro_route not a valid Accel Synchro route (counterspelled, reaction, main action)
-	ValueError
 		source_spell was not found in the database.
 	ValueError
 		source_spell has multiple matches in the database.
-
-	TODO: add 3 different queries to handle the different synchro routes
 	"""
-	if not synchro_route in range(3):
-		raise ValueError(
-			f"{synchro_route} not a valid Accel Synchro route (counterspelled: 0, reaction: 1, main action: 2)"
-			)
-
 	with sqlite3.connect("../Gestalt.db") as connection:
 		gestalt_cursor = connection.cursor()
 		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_name LIKE ?", [f"%{source_spell}%"])
@@ -44,37 +40,126 @@ def find_Accel_Synchro_target(source_spell: str, synchro_route: int):
 
 		source_spell = check_provided_spell[0] # load spell tuple from db
 
-		spell_level = source_spell[2]
-		spell_school = source_spell[5]
-
-		route_constraints = ""
-
-		if synchro_route == 0: # Synchro spell has just been counterspelled
-			route_constraints = """
+		gestalt_cursor.execute(
+			"""
+			SELECT 
+				* FROM Spell 
+			WHERE 
 				(casting_time == "Action" OR casting_time == "Bonus Action")
 				AND level > ?
 				AND school == ?
-				"""
-		# could potentially add a check here to see if the input spell is a valid
-		# source spell (duration != "Instantaneous"). Spells are weird so we
-		# might just have to trust the caller.
-		elif synchro_route == 1: # ending active spell to cast a reaction spell
-			route_constraints = """
-				casting_time == "Reaction"
-				AND level > ?
-				AND school == ?
-				"""
-		elif synchro_route == 2: # using main action, no limit on casting time
-			route_constraints = """
-				level > ?
-				AND school == ?
-				"""
-
-		gestalt_cursor.execute(f"SELECT * FROM Spell WHERE {route_constraints}", (spell_level, spell_school))
+			""",
+			[source_spell[2], source_spell[5]] # [level, school]
+		)
 
 		return gestalt_cursor.fetchall()
 
+
+def get_reaction_Accel_Synchro(source_spell: str):
+	"""
+	Given a spell with a duration greater than instantaneous, determine the
+	eligible Reaction spells to Accel Synchro into.
+
+	Parameters
+	----------
+	source_spell: str
+		Completely or partially matching unique spell name within the database.
+
+	Returns
+	-------
+	target_spells: list[tuple]
+		List of tuples of Reaction Accel Synchro target spells.
+
+	Raises
+	------
+	ValueError
+		source_spell was not found in the database.
+	ValueError
+		source_spell has multiple matches in the database.
+	"""
+	with sqlite3.connect("../Gestalt.db") as connection:
+		gestalt_cursor = connection.cursor()
+		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_name LIKE ?", [f"%{source_spell}%"])
+
+		check_provided_spell = gestalt_cursor.fetchall()
+
+		# important that our query finds 1 and only 1 result
+		if not check_provided_spell:
+			raise ValueError("source_spell not found.")
+		
+		elif len(check_provided_spell) > 1:
+			raise ValueError(f"Multiple spells found with the provided spell name: {[spell[1] for spell in check_provided_spell]}.")
+
+		source_spell = check_provided_spell[0] # load spell tuple from db
+
+		gestalt_cursor.execute(
+			"""
+			SELECT 
+				* FROM Spell 
+			WHERE 
+				casting_time == "Reaction"
+				AND level > ?
+				AND school == ?
+			""",
+			[source_spell[2], source_spell[5]] # [level, school]
+		)
+
+		return gestalt_cursor.fetchall()
+
+
+def get_main_action_Accel_Synchro():
+	"""
+	Given a spell with a duration greater than instantaneous, determine the
+	eligible Reaction spells to Accel Synchro into.
+
+	Parameters
+	----------
+	source_spell: str
+		Completely or partially matching unique spell name within the database.
+
+	Returns
+	-------
+	target_spells: list[tuple]
+		List of tuples of Reaction Accel Synchro target spells.
+
+	Raises
+	------
+	ValueError
+		source_spell was not found in the database.
+	ValueError
+		source_spell has multiple matches in the database.
+	"""
+	with sqlite3.connect("../Gestalt.db") as connection:
+		gestalt_cursor = connection.cursor()
+		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_name LIKE ?", [f"%{source_spell}%"])
+
+		check_provided_spell = gestalt_cursor.fetchall()
+
+		# important that our query finds 1 and only 1 result
+		if not check_provided_spell:
+			raise ValueError("source_spell not found.")
+		
+		elif len(check_provided_spell) > 1:
+			raise ValueError(f"Multiple spells found with the provided spell name: {[spell[1] for spell in check_provided_spell]}.")
+
+		source_spell = check_provided_spell[0] # load spell tuple from db
+
+		gestalt_cursor.execute(
+			"""
+			SELECT 
+				* FROM Spell 
+			WHERE 
+				level > ?
+				AND school == ?
+			""",
+			[source_spell[2], source_spell[5]] # [level, school]
+		)
+
+		return gestalt_cursor.fetchall()
+
+
 	return
+
 
 def find_Fusion_target():
 	"""
