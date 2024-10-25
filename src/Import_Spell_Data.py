80e85f66d14e5184a26f28cec4f96da41724f335
EbbyRaymundo <ebraymundo24@gmail.com>
2024-10-25 04:14:59 -0400

    WIP polars import mode, spell stats with timings
    Began using polars to import the spell data. Currently, it is not properly replacing the minute(s) in the duration column and is not properly appending a concentration and ritual column to the result.

--- src/Import_Spell_Data.py
+++ src/Import_Spell_Data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import polars as pl
 
 
 
@@ -210,6 +211,151 @@ def format_spell_csv(csv_name: str):
 
 	return spell_table, dnd_classes, all_availability
 
+def format_spell_csv_pl(csv_name: str):
+
+	'''
+	Read the csv from 5etools containing all spells currently available on the website,
+	clean entries to remove unnecessary information or create additional columns,
+	create Class and Spell_Class junction table.
+
+	To format the csv for the database:
+	- Rename column names to those matching the Gestalt ERD
+	- Remap all string names in "level" to their corresponding spell level as integers (cantrips == 0)
+	- Abbreviate "hour(s)" and "minute(s)" in "duration" column
+	- Remove "Concentration, " from "duration" and give it its own boolean column
+	- Remove " (ritual)" from "school" and give it its own boolean column
+	- Abbreviate "foot", "feet", "mile(s)" in "range" column
+	- Listify "character_class" and "optional_classes" column, expand them, pivot longer, then full outer join
+	- Remove "At Higher Levels. " from "upcast_effect"
+	- Create a table with all spellcasting classes (except you Monks that only have one spell)
+		- Subclasses are not considered since that data is irrelevant for to the purpose of the database
+	- Create a junction table between the spells and classes
+	'''
+
+	spell_table = (
+		pl.scan_csv(
+			source = csv_name,
+			has_header = True,
+			new_columns = 
+				[
+					"spell_name",
+					"level",
+					"casting_time",
+					"duration",
+					"school",
+					"range",
+					"components",
+					"character_class",
+					"optional_classes",
+					"description",
+					"upcast_effect"
+				]
+		)
+		.with_columns(
+			concentration = pl.col("duration").str.contains("Concentration"),
+			ritual = pl.col("school").str.contains("ritual")
+		)
+		.select(
+			pl.col("spell_name").str.strip_chars(),
+			pl.col("level").str.strip_chars().str.replace_many(
+				{
+					"Cantrip": '0',
+					"1st": '1',
+					"2nd": '2',
+					"3rd": '3',
+					"4th": '4',
+					"5th": '5',
+					"6th": '6',
+					"7th": '7',
+					"8th": '8',
+					"9th": '9'
+				}
+			),
+			pl.col("casting_time").str.strip_chars().str.replace_all(
+				pattern = "Bonus", 
+				value = "Bonus Action"),
+			pl.col("duration").str.strip_chars().str.strip_prefix("Concentration, ").str.replace_all(
+				pattern = "minute(s)?",
+				value = "Min."
+			)
+		)
+	)
+	
+	'''
+	spell_table.rename_axis("spell_id", inplace = True)
+
+	# useless text; "At Higher Levels" in an upcast column is just redundant
+	spell_table["upcast_effect"] = spell_table["upcast_effect"].str.removeprefix("At Higher Levels. ").str.strip()
+
+	# make boolean concentration column and clean up afterwards
+	spell_table["concentration"] = spell_table["duration"].str.contains("Concentration")
+	spell_table["duration"] = spell_table["duration"].str.removeprefix("Concentration, ").str.capitalize().str.strip()
+
+	# "casting_time" is already abbreviated, so we'll be consistent with abbreviations
+	spell_table["duration"] = spell_table["duration"].str.replace(pat = "minute(s)?", repl = "Min.", regex = True)
+	spell_table["duration"] = spell_table["duration"].str.replace(pat = "hour(s)?", repl = "Hr.", regex = True)
+
+	# chose to use abbreviations in the "duration" column so I should be consistent
+	spell_table["range"] = spell_table["range"].str.replace(pat = "feet|foot", repl = "Ft.", regex = True)
+	spell_table["range"] = spell_table["range"].str.replace(pat = "mile(s)?", repl = "Mi.", regex = True)
+
+	# make the boolean ritual column and clean up afterwards
+	spell_table["ritual"] = spell_table["school"].str.contains("ritual")
+	spell_table["school"] = spell_table["school"].str.removesuffix(" (ritual)").str.strip()
+
+
+	# construct DataFrame for the Class table
+	dnd_classes = pd.DataFrame(data = ["Wizard", "Cleric", "Sorcerer", "Bard", "Druid", "Artificer", "Paladin", "Warlock", "Ranger"], columns = ["character_class"])
+	dnd_classes.rename_axis("character_class_id", inplace = True)
+
+	# listify, expand, and merge the class_availability with its corresponding spell_id
+
+	# listify and expand the classes column
+	class_availability = spell_table["character_class"].str.split(", ", expand = True)
+	class_availability = pd.concat([spell_table.index.to_series(), class_availability], axis = "columns")
+	class_availability = class_availability.melt(id_vars = "spell_id", value_name = "character_class").drop("variable", axis = "columns") # pivot longer
+	class_availability.dropna(inplace = True)
+
+	# repeat for the optional_availability column
+	optional_availability = spell_table["optional_classes"].str.split(", ", expand = True) # listify and expand
+	optional_availability = pd.concat([spell_table.index.to_series(), optional_availability], axis = "columns")
+	optional_availability = optional_availability.melt(id_vars = "spell_id", value_name = "character_class").drop("variable", axis = "columns") # pivot longer
+	optional_availability.dropna(inplace = True)
+
+	all_availability = pd.merge(class_availability, optional_availability, how = "outer")
+
+	all_availability.replace(to_replace = "", value = pd.NA, inplace = True)
+	all_availability.dropna(inplace = True)
+
+	# this replaces with the corresponding character_class_id in
+	# the dnd_classes dataframe
+	all_availability = all_availability.replace(
+		{"character_class": {
+			"Wizard": '0',
+			"Cleric": '1',
+			"Sorcerer": '2',
+			"Bard": '3',
+			"Druid": '4',
+			"Artificer": '5',
+			"Paladin": '6',
+			"Warlock": '7',
+			"Ranger": '8'
+			}
+   		}
+	).astype({"character_class": "int64"}, copy = False)
+
+	all_availability.reset_index(drop = True, inplace = True) # index is spotty from the dropna()
+	# need to appropriately rename the column after remapping
+	all_availability.rename(columns = {"character_class": "character_class_id"}, inplace = True)
+	
+	spell_table.drop(columns = ["character_class", "optional_classes"], inplace = True) # new tables made, no longer needed
+	# empty upcast values were never filled with NAs by pandas in read_csv for some reason
+	spell_table.replace(to_replace = "", value = pd.NA, inplace = True)
+
+	'''
+
+	return spell_table.collect()
+
 
 def import_default_xyz(csv_name: str):
 	'''
@@ -285,8 +431,8 @@ def main():
 	#format_spell_csv("../spell_data/all_5e_spells.csv")
 	#import_default_xyz("../spell_data/kites_xyz_spells.csv")
 	#import_default_links("../spell_data/aleisters_link_spells.csv")
-
-
+	polar_spells = format_spell_csv_pl("../spell_data/all_5e_spells.csv")
+	print(polar_spells.head(10))
 
 	return 0
 
