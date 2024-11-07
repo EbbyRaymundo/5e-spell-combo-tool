53c821aa8ddb0a777101d57e3094fc65a7254604
EbbyRaymundo <ebraymundo24@gmail.com>
2024-11-06 21:31:09 -0500

    Added connection to Gestalt.db writes

--- src/Populate_Gestalt.py
+++ src/Populate_Gestalt.py
@@ -3,6 +3,7 @@ import polars as pl
 import Import_Spells
 
 def populate_main_tables(
+		connection: adbc.Connection,
 		spell_DataFrame: pl.DataFrame,
 		class_DataFrame: pl.DataFrame,
 		fusion_DataFrame: pl.DataFrame,
@@ -14,16 +15,37 @@ def populate_main_tables(
 	created in Build_Gestalt.py.
 	'''
 
-	spell_DataFrame.write_database(name = "Spell", if_table_exists = "append")
-	class_DataFrame.write_database(name = "Class", if_table_exists = "append")
+	spell_DataFrame.write_database(
+		table_name = "Spell",
+		connection = connection,
+		if_table_exists = "append"
+	)
+	class_DataFrame.write_database(
+		table_name = "Class",
+		connection = connection,
+		if_table_exists = "append"
+	)
 	# TODO: implement fusion_DataFrame function
-	fusion_DataFrame.write_database(name = "Fusion", if_table_exists = "append")
-	xyz_DataFrame.write_database(name = "XYZ", if_table_exists = "append")
-	link_DataFrame.write_database(name = "Link", if_table_exists = "append")	
+	fusion_DataFrame.write_database(
+		table_name = "Fusion",
+		connection = connection,
+		if_table_exists = "append"
+	)
+	xyz_DataFrame.write_database(
+		table_name = "XYZ",
+		connection = connection,
+		if_table_exists = "append"
+	)
+	link_DataFrame.write_database(
+		table_name = "Link",
+		connection = connection,
+		if_table_exists = "append"
+	)	
 
 	return 0
 
 def populate_junction_tables(
+		connection: adbc.Connection,
 		spell_class_DataFrame: pl.DataFrame,
 		spell_fusion_DataFrame: pl.DataFrame,
 		xyz_class_DataFrame: pl.DataFrame,
@@ -34,37 +56,59 @@ def populate_junction_tables(
 	created in Build_Gestalt.py.
 	'''
 
-	spell_class_DataFrame.write_database(name = "Spell_Class", if_table_exists = "append")
+	spell_class_DataFrame.write_database(
+		table_name = "Spell_Class",
+		connection = connection,
+		if_table_exists = "append"
+	)
 	# TODO: implement
-	spell_fusion_DataFrame.write_database(name = "Spell_Fusion", if_table_exists = "append")
-	xyz_class_DataFrame.write_database(name = "XYZ_Class", if_table_exists = "append")
-	link_class_DataFrame.write_database(name = "Link_Class", if_table_exists = "append")
+	spell_fusion_DataFrame.write_database(
+		table_name = "Spell_Fusion",
+		connection = connection,
+		if_table_exists = "append"
+	)
+	xyz_class_DataFrame.write_database(
+		table_name = "XYZ_Class",
+		connection = connection,
+		if_table_exists = "append"
+	)
+	link_class_DataFrame.write_database(
+		table_name = "Link_Class",
+		connection = connection,
+		if_table_exists = "append"
+	)
 
 	return 0
 
 
 def main():
 
-	spell_table, class_table, spell_class_table = Import_Spells.format_spell_csv("../spell_data/all_5e_spells.csv")
-	fusion_table, spell_fusion_table = Import_Spells.import_default_fusions("../spell_data/aleisters_fusion_spells.csv")
-	xyz_table, xyz_class_table = Import_Spells.import_default_xyz("../spell_data/kites_xyz_spells.csv")
-	link_table, link_class_table = Import_Spells.import_default_links("../spell_data/aleisters_link_spells.csv")
+	with adbc.connect("../Gestalt.db") as connection:
 
-	populate_main_tables(
-		spell_table,
-		class_table,
-		fusion_table,
-		xyz_table,
-		link_table,
-		
-	)
+		connection.execute("PRAGMA foreign_keys = ON")
 
-	populate_junction_tables(
-		spell_class_table,
-		spell_fusion_table,
-		xyz_class_table,
-		link_class_table
-	)
+		spell_table, class_table, spell_class_table = Import_Spells.format_spell_csv("../spell_data/all_5e_spells.csv")
+		fusion_table, spell_fusion_table = Import_Spells.import_default_fusions("../spell_data/aleisters_fusion_spells.csv")
+		xyz_table, xyz_class_table = Import_Spells.import_default_xyz("../spell_data/kites_xyz_spells.csv")
+		link_table, link_class_table = Import_Spells.import_default_links("../spell_data/aleisters_link_spells.csv")
+
+		populate_main_tables(
+			connection,
+			spell_table,
+			class_table,
+			fusion_table,
+			xyz_table,
+			link_table,
+			
+		)
+
+		populate_junction_tables(
+			connection,
+			spell_class_table,
+			spell_fusion_table,
+			xyz_class_table,
+			link_class_table
+		)
 
 	return 0
 
