2b2d0153e168c82ecfe16073ff27655cc4336c8d
EbbyRaymundo <ebraymundo24@gmail.com>
2024-11-07 20:32:31 -0500

    Removed previous sqlite3 implementations.

--- src/Query_Gestalt.py
+++ src/Query_Gestalt.py
@@ -8,197 +8,36 @@ Notes to self:
 - Prefix query with EXPLAIN QUERY PLAN to check if any queries
   have to scan across a table.
 """
+
+
+
 # TODO: decide if I want to add error handling to the Accel Synchro getters to
 #		check if the input spell has a duration greater than Instantaneous.
 # TODO: change the getters to handle a list of int spell_id's instead of a single input.
-def get_counterspelled_Accel_Synchro(spell_id: int):
-	'''
-	Given the spell_id of a spell with a duration longer than "Instantaneous", 
-	determine the eligible Reaction spells to Accel Synchro into.
-
-	Parameters
-	----------
-	spell_id: int
-
-	Returns
-	-------
-	target_spells: list[tuple]
-		List of tuples of counterspelled Accel Synchro target spells.
-
-	Raises
-	------
-	ValueError
-		Provided spell_id not found in Spell table
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-
-		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_id == ?", spell_id)
-		source_spell = gestalt_cursor.fetchall()
-
-		if not source_spell:
-			raise ValueError(f"spell_id {spell_id} not found.")
+def get_counterspelled_Accel_Synchro(connection, spell_id: int):
 		
-		source_spell = gestalt_cursor.fetchall()
-		source_spell = source_spell[0] # .fetchall() result is in format [(spell)]
-
-		gestalt_cursor.execute(
-			"""
-			SELECT 
-				* FROM Spell 
-			WHERE 
-				(casting_time == "Action" OR casting_time == "Bonus Action")
-				AND level > ?
-				AND school == ?
-			""",
-			[source_spell[2], source_spell[5]] # [level, school]
-		)
-
-		return gestalt_cursor.fetchall()
+		return
 
 
-def get_reaction_Accel_Synchro(spell_id: int):
-	'''
-	Given a spell with a duration greater than Instantaneous, determine the
-	eligible Reaction spells to Accel Synchro into.
+def get_reaction_Accel_Synchro(connection, spell_id: int):
 
-	Parameters
-	----------
-	source_spell: int
-		spell_id of the source spell.
-	
-	Returns
-	-------
-	target_spells: list[tuple]
-		List of tuples of Reaction Accel Synchro target spells.
-
-	Raises
-	------
-	ValueError
-		Provided spell_id not found in Spell table
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-
-		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_id == ?", spell_id)
-		source_spell = gestalt_cursor.fetchall()
-
-		if not source_spell:
-			raise ValueError(f"spell_id {spell_id} not found.")
-		
-		source_spell = gestalt_cursor.fetchall()
-		source_spell = source_spell[0] # .fetchall() result is in format [(spell)]
-
-		gestalt_cursor.execute(
-			"""
-			SELECT 
-				* FROM Spell 
-			WHERE 
-				casting_time == "Reaction"
-				AND level > ?
-				AND school == ?
-			""",
-			[source_spell[2], source_spell[5]] # [level, school]
-		)
-
-		return gestalt_cursor.fetchall()
+		return
 
 
-def get_main_action_Accel_Synchro(spell_id: int):
-	'''
-	Given the spell_id of a spell with a duration greater than Instantaneous,
-	determine the eligible spells to Accel Synchro into.
+def get_main_action_Accel_Synchro(connection, spell_id: int):
 
-	Parameters
-	----------
-	spell_id: int
-	
-	Returns
-	-------
-	target_spells: list[tuple]
-		List of tuples of Reaction Accel Synchro target spells.
-
-	Raises
-	------
-	ValueError
-		Provided spell_id not found in Spell table
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-
-		gestalt_cursor.execute("SELECT * FROM Spell WHERE spell_id == ?", spell_id)
-		source_spell = gestalt_cursor.fetchall()
-
-		if not source_spell:
-			raise ValueError(f"spell_id {spell_id} not found.")
-
-		source_spell = source_spell[0] # .fetchall() result is in format [(spell)]
-
-		gestalt_cursor.execute(
-			"""
-			SELECT 
-				* FROM Spell 
-			WHERE 
-				level > ?
-				AND school == ?
-			""",
-			[source_spell[2], source_spell[5]] # [level, school]
-		)
-
-		return gestalt_cursor.fetchall()
+		return
 
 
-def get_Fusion_targets(spell_id: int):
-	'''
-	Given a spell_id, return any available Fusion spell targets and their
-	spell components, EXCEPT for the input spell.
-
-	Parameters
-	----------
-	spell_id: int
-
-	Returns
-	-------
-	target_fusions: list[tuple]
-		List of query results with tuples in format (Fusion.*, Spell.*)
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-
-		gestalt_cursor.execute(
-			"""
-			SELECT * FROM Fusion
-			JOIN Spell_Fusion ON Fusion.fusion_id = Spell_Fusion.fusion_id
-			JOIN Spell ON Spell_Fusion.spell_id = Spell.spell_id
-			WHERE Fusion.fusion_id IN (
-   				SELECT fusion_id
-   				FROM Spell_Fusion
-   				WHERE spell_id == ?
-   			)
-			AND Spell.spell_id != ?
-			""",
-			[spell_id, spell_id]
-		)
-
-		# tuples containing fusions using input spell_id as components
-		# and the OTHER spells required as their components
-		target_fusions = gestalt_cursor.fetchall()
-
-		if not target_fusions:
-			raise ValueError(f"spell_id {spell_id} not found as a Fusion component.")
-
-		return target_fusions
+def get_Fusion_targets(connection, spell_id: int):
 
 	return
 
 
 def main():
 
-	fireball_Fusions = get_Fusion_targets(181)
-
-	print(fireball_Fusions)
-
 	return 0
+
 
 
 if __name__ == "__main__":
--- src/Update_Gestalt.py
+++ src/Update_Gestalt.py
@@ -3,223 +3,29 @@ import adbc_driver_sqlite.dbapi as adbc
 
 
 
-def add_spell(spell: tuple[str, int, str, str, str, str, str, str, str, bool, bool], character_classes: list[int] = None):
-	'''
-	Add the standard spell into the Spell table and associate
-	the spell with every character class unless provided a list
-	to specify otherwise.
-	Tuple representing spell should be in format:
+def add_spell(connection, spell, character_classes: list[int] = None):
 
-	(spell_name, level, casting_time, duration, school, range, components, description, upcast_effect, concentration, ritual)
-
-	spell_name: str, in title case
-	level: int
-	casting_time: str, being "Action", "Bonus Action", "Reaction", or an amount of time being abbreviated (Min./Hr.)
-	duration: str, with the time being abbreviated (Min./Hr.).
-	school: str, capitalized.
-	range: str, with the distance being abbreviation (Ft./Mi.).
-	components: str, in format "V, S, M (<material components>).
-	description: str.
-	upcast_effect: str or None, description at higher levels, None type otherwise.
-	concentration: bool.
-	ritual: bool.
-
-	Parameters
-	----------
-	spell: tuple[str, int, str, str, str, str, str, str, str, bool, bool]
-	character_classes: list[int]
-		character_class_id's of the classes the spell is available to. 
-		Associated with all spellcasting classes if no list given.
-
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-		gestalt_cursor.execute("PRAGMA foreign_keys = ON")
-		
-		gestalt_cursor.execute(
-			"""
-			INSERT INTO Spell(spell_name, level, casting_time, duration, school, range, components, description, upcast_effect, concentration, ritual) 
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-			""",
-			spell
-			)
-
-		new_spell_id = gestalt_cursor.lastrowid
-		spell_class_ids = associate_classes(new_spell_id, character_classes, gestalt_cursor)
-
-		gestalt_cursor.executemany("INSERT INTO Spell_Class VALUES (?, ?)", spell_class_ids)
-
-		return new_spell_id
+	return
 
 
+def add_XYZ(connection, xyz, character_classes: list[int] = None):
 
-	return 0
-
-
-def add_XYZ(xyz: tuple[str, int, str, str, str, str, str], character_classes: list[int] = None):
-	'''
-	Add the XYZ spell into the XYZ table and associate the XYZ
-	spell with every character class unless provided a list to
-	specify otherwise.
-	Tuple representing XYZ should be in format:
-
-	(XYZ_name, rank, casting_time, duration, range, components, description)
-
-	xyz_name: str, in title case.
-	rank: int representing XYZ rank (1-9 typically).
-	casting_time: str representing the action economy to cast (typically "Action and Bonus Action").
-	duration: str, with the time being abbreviated (Min./Hr.). Typically last 1 Hr.
-	range: str, with the distance being abbreviated (Ft./Mi.). Can also be "Line of sight".
-	components: str, typically "V, S, M (2 level <rank> spell slots and <rank> sorcery points)".
-				Can also specify "May also use a pre-existing <XYZ or spell_name> and n 
-				sorcery points" if you can overlay from an existing spell into this XYZ.
-				Example: "V, S, M (2 level 4 spell slots and 4 sorcery points. May also
-						  also use a pre-existing 'Sparks' XYZ spell and 2 sorcery points)".
-	description: str, typically in format of continuous effect, then the effect that occurs
-				 when you detach a material.
-
-	Parameters
-	----------
-	XYZ: tuple[str, int, str, str, str, str, str]
-	character_classes: list[int]
-		character_class_id's of the classes the XYZ is available to. 
-		Associated with all full casters if no list given.
-
-	Returns
-	-------
-	new_XYZ_id: int
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-		gestalt_cursor.execute("PRAGMA foreign_keys = ON")
-		# XYZ have no spell school, do not use concentration, can't be rituals, and have no upcast effects
-		gestalt_cursor.execute(
-			"""
-			INSERT INTO XYZ(xyz_name, rank, casting_time, duration, range, components, description) 
-			VALUES (?, ?, ?, ?, ?, ?, ?)
-			""",
-			xyz
-			)
-
-		new_xyz_id = gestalt_cursor.lastrowid
-
-		# if no specifying class list given, use default character_class_id's for
-		# XYZ spells (any full caster class that can obtain 9th lvl spells)
-		if not character_classes:
-			character_classes = list(*range(5)) # id's: [Wizard, Cleric, Sorc, Bard, Druid]
-
-		xyz_class_ids = associate_classes(new_xyz_id, character_classes, gestalt_cursor)
-
-		gestalt_cursor.executemany("INSERT INTO XYZ_Class VALUES (?, ?)", xyz_class_ids)
-
-		return new_xyz_id
+	return
 
 
-def add_Link(link: tuple[str, int, str, str, str, str, str], character_classes: list[int] = None):
-	"""
-	Add the Link spell into the Spell table and associate the Link
-	spell with every character class unless provided a list to specify.
-	Tuple representing the Link should be in format:
+def add_Link(connection, link, character_classes: list[int] = None):
 
-	(<link_name>, <rating>, <duration>, <range>, <components>, <description>)
-
-	link_name: str, in title case.
-	rating: int representing the amount of materials required to cast the Link spell.
-	casting_time: str, typically "Action"
-	duration: str, with the time being abbreviated (Min./Hr.). Typically last 1 Min. * Link rating.
-	range: str, with the distance being abbreviated (Ft./Mi.). Can also be "Line of Sight".
-	components: str, typically "V, S, M (<Link materials>)".
-				Example: "V, S, M (1 Synchro spell and 1 spell)"
-	description: str, typically in format "As an Action, link <Link materials>", then specify
-				 the Link effect.
-
-	Parameters
-	----------
-	Link: tuple[str, int, str, str, str, str, str]
-	character_classes: list[int]
-		character_class_id's of the classes the Link is available to. 
-		Associated with all classes if no list given. Links are typically
-		associated with ALL spellcasting classes and only have an
-		Intelligence attribute requirement.
-
-	Returns
-	-------
-	new_Link_id: int
-	"""
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-		gestalt_cursor.execute("PRAGMA foreign_keys = ON")
-		# XYZ have no spell school, do not use concentration, can't be rituals, and have no upcast effects
-		gestalt_cursor.execute(
-			"""
-			INSERT INTO Link(link_name, rating, casting_time, duration, range, components, description) 
-			VALUES (?, ?, ?, ?, ?, ?, ?)
-			""",
-			link
-			)
-
-		new_link_id = gestalt_cursor.lastrowid
-		link_class_ids = associate_classes(new_link_id, character_classes, gestalt_cursor)
-
-		gestalt_cursor.executemany("INSERT INTO Link_Class VALUES (?, ?)", link_class_ids)
-
-		return new_link_id
+	return
 
 
-def add_Fusion(fusion: tuple[str, str, str, str, bool], constituent_spells: list[int]):
-	'''
-	Add the Fusion into the Fusion table and associate the Fusion with all of
-	its constituent spells in the Spell_Fusion table.
-	Tuple representing fusion should be in format:
+def add_Fusion(connection, fusion, constituent_spells: list[int]):
 
-	(fusion_name, duration, range, description, concentration)
-
-	spell_name: str, in title case
-	duration: str, with the time being abbreviated (Min./Hr.).
-	range: str, with the distance being abbreviation (Ft./Mi.).
-	description: str.
-	concentration: bool.
-
-	Parameters
-	----------
-	fusion: list[str, str, str, str, bool]
-	constituent_spells: list[int]
-		spell_id's of spells used to create the fusion.
-
-	Returns
-	-------
-	new_fusion_id: int
-	'''
-	with adbc.connect("../Gestalt.db") as connection:
-		gestalt_cursor = connection.cursor()
-
-		gestalt_cursor.execute("PRAGMA foreign_keys = ON")
-		gestalt_cursor.execute("INSERT INTO Fusion(fusion_name, duration, range, description, concentration) VALUES (?, ?, ?, ?, ?)", fusion)
-
-		new_Fusion_id = gestalt_cursor.lastrowid
-
-		gestalt_cursor.executemany(
-			"INSERT INTO Spell_Fusion VALUES (?, ?)",
-			[(spell_id, new_Fusion_id) for spell_id in constituent_spells]
-			)
-
-		return new_Fusion_id
+	return
 
 
-def associate_classes(spell_id: int, character_classes: list[int], gestalt_cursor: adbc.Cursor):
-	"""
-	Generate a list of [spell_id, character_class_id] lists
-	to associate a spell with character classes. Associates
-	a spell with every class if character_classes = None.
-	"""
-	if not character_classes: # passed an empty list, associate with all classes
-		gestalt_cursor.execute("SELECT character_class_id FROM Class")
+def associate_classes(connection, spell_id: int, character_classes: list[int]):
 
-		# unpack the result to match the character_classes param
-		character_classes = [character_class[0] for character_class in gestalt_cursor.fetchall()]
-
-	# return our list of [spell_id, character_class_id] lists
-	return [[spell_id, character_class_id] for character_class_id in character_classes]
+	return
 
 	
 def main():
