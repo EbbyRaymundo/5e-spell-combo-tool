257413bf790673370055669cb604816e6389cf50
EbbyRaymundo <ebraymundo24@gmail.com>
2024-11-09 14:00:36 -0500

    Edit docstrings, revert functions to sqlite3 implementations
    This file needs to be rewritten in pandas functions in the future. It is not my intention to keep these as sqlite3 implementations but they'll stay that way for the time being.

--- src/Update_Gestalt.py
+++ src/Update_Gestalt.py
@@ -1,11 +1,18 @@
+import sqlite3
+import pandas as pd
+
 '''
 This file adds new spells into Gestalt.db and handles
 the appropriate junction table insertions.
 '''
 
 
-
-def add_spell(connection, spell, character_classes: list[int] = None):
+#TODO: convert to pandas implementation
+def add_spell(
+	connection: sqlite3.Connection,
+	spell,
+	character_classes: list[int] = None
+):
 	'''
 	Add the standard spell into the Spell table and associate
 	the spell with every character class unless provided a list
@@ -29,7 +36,7 @@ def add_spell(connection, spell, character_classes: list[int] = None):
 
 	Parameters
 	----------
-	connection:
+	connection: sqlite3.Connection
 		Database connection to Gestalt.db.
 	spell:
 		New spell in the format specified above and in the Spell table
@@ -37,12 +44,34 @@ def add_spell(connection, spell, character_classes: list[int] = None):
 	character_classes: list[int]
 		character_class_id's of the classes the spell is available to. 
 		Associated with all spellcasting classes if no list given.
+
+	Returns
+	-------
+	new_spell_id: int
 	'''
+	gestalt_cursor = connection.cursor()
+	gestalt_cursor.execute("PRAGMA foreign_keys = ON")
+	
+	gestalt_cursor.execute(
+		"""
+		INSERT INTO Spell(spell_name, level, casting_time, duration, school, range, components, description, upcast_effect, concentration, ritual) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		""",
+		spell
+	)
+	
+	new_spell_id = gestalt_cursor.lastrowid
+
+	spell_class_ids = associate_classes(new_spell_id, character_classes, gestalt_cursor)
+
+	gestalt_cursor.executemany("INSERT INTO Spell_Class VALUES (?, ?)", spell_class_ids)
+
+	return new_spell_id
 
 	return
 
 
-def add_XYZ(connection, xyz, character_classes: list[int] = None):
+def add_XYZ(connection: sqlite3.Connection, xyz, character_classes: list[int] = None):
 	'''
 	Add the XYZ spell into the XYZ table and associate the XYZ
 	spell with every character class unless provided a list to
@@ -66,7 +95,7 @@ def add_XYZ(connection, xyz, character_classes: list[int] = None):
 
 	Parameters
 	----------
-	connection:
+	connection: sqlite.Connection
 		Database connection to Gestalt.db.
 	XYZ:
 		New XYZ spell in the format specified above and in the XYZ table
@@ -79,11 +108,33 @@ def add_XYZ(connection, xyz, character_classes: list[int] = None):
 	-------
 	new_XYZ_id: int
 	'''
+	gestalt_cursor = connection.cursor()
+	gestalt_cursor.execute("PRAGMA foreign_keys = ON")
 
-	return
+	# XYZ have no spell school, do not use concentration, can't be rituals, and have no upcast effects
+	gestalt_cursor.execute(
+		"""
+		INSERT INTO XYZ(xyz_name, rank, casting_time, duration, range, components, description) 
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+		""",
+		xyz
+	)
+	
+	new_xyz_id = gestalt_cursor.lastrowid
+
+	# if no specifying class list given, use default character_class_id's for
+	# XYZ spells (any full caster class that can obtain 9th lvl spells)
+	if not character_classes:
+		character_classes = list(*range(5)) # id's: [Wizard, Cleric, Sorc, Bard, Druid]
+
+	xyz_class_ids = associate_classes(new_xyz_id, character_classes, gestalt_cursor)
+
+	gestalt_cursor.executemany("INSERT INTO XYZ_Class VALUES (?, ?)", xyz_class_ids)
+
+	return new_xyz_id
 
 
-def add_Link(connection, link, character_classes: list[int] = None):
+def add_Link(connection: sqlite3.Connection, link, character_classes: list[int] = None):
 	'''
 	Add the Link spell into the Spell table and associate the Link
 	spell with every character class unless provided a list to specify.
@@ -103,6 +154,8 @@ def add_Link(connection, link, character_classes: list[int] = None):
 
 	Parameters
 	----------
+	connection: sqlite3.Connection
+		Database connection to Gestalt.db.
 	Link:
 		New Link spell in the format specified above and in the Link table
 		within Gestalt_ERD.png.
@@ -116,11 +169,28 @@ def add_Link(connection, link, character_classes: list[int] = None):
 	-------
 	new_Link_id: int
 	'''
+	gestalt_cursor = connection.cursor()
+	gestalt_cursor.execute("PRAGMA foreign_keys = ON")
 
-	return
+	# XYZ have no spell school, do not use concentration, can't be rituals, and have no upcast effects
+	gestalt_cursor.execute(
+		"""
+		INSERT INTO Link(link_name, rating, casting_time, duration, range, components, description) 
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+		""",
+		link
+	)
+
+	new_link_id = gestalt_cursor.lastrowid
+
+	link_class_ids = associate_classes(new_link_id, character_classes, gestalt_cursor)
+
+	gestalt_cursor.executemany("INSERT INTO Link_Class VALUES (?, ?)", link_class_ids)
+
+	return new_link_id
 
 
-def add_Fusion(connection, fusion, constituent_spells: list[int]):
+def add_Fusion(connection: sqlite3.Connection, fusion, constituent_spells: list[int]):
 	'''
 	Add the Fusion into the Fusion table and associate the Fusion with all of
 	its constituent spells in the Spell_Fusion table.
@@ -136,8 +206,8 @@ def add_Fusion(connection, fusion, constituent_spells: list[int]):
 
 	Parameters
 	----------
-	connection:
-		database connection to Gestalt.db.
+	connection: sqlite3.Connection
+		Database connection to Gestalt.db.
 	fusion:
 		New Fusion spell in the format specified above and in the Fusion table
 		within Gestalt_ERD.png.
@@ -148,18 +218,41 @@ def add_Fusion(connection, fusion, constituent_spells: list[int]):
 	-------
 	new_fusion_id: int
 	'''
+	gestalt_cursor = connection.cursor()
+	gestalt_cursor.execute("PRAGMA foreign_keys = ON")
 
-	return
+	gestalt_cursor.execute(
+		"""
+		INSERT INTO 
+		Fusion(fusion_name, duration, range, description, concentration)
+		VALUES (?, ?, ?, ?, ?)
+		""",
+		fusion
+	)
+
+	gestalt_cursor.executemany(
+	"INSERT INTO Spell_Fusion VALUES (?, ?)",
+	[(spell_id, new_Fusion_id) for spell_id in constituent_spells]
+	)
+
+	new_Fusion_id = gestalt_cursor.lastrowid
+
+	return new_Fusion_id
 
 
-def associate_classes(connection, spell_id: int, character_classes: list[int]):
+def associate_classes(cursor: sqlite3.Cursor, spell_id: int, character_classes: list[int]):
 	'''
 	Generate a result of [spell_id, character_class_id] lists
 	to associate a spell with character classes. Associates
 	a spell with every class if character_classes = None.
 	'''
+	if not character_classes: # passed an empty list, associate with all classes
+		cursor.execute("SELECT character_class_id FROM Class")
+	# unpack the result to match the character_classes param
+	character_classes = [character_class[0] for character_class in cursor.fetchall()]
 
-	return
+	# return our list of [spell_id, character_class_id] lists
+	return [[spell_id, character_class_id] for character_class_id in character_classes]
 
 	
 def main():
