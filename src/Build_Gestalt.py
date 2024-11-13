b6d454e751f2616c0e7dc0bf3332bc2f34a0cf9c
EbbyRaymundo <ebraymundo24@gmail.com>
2024-11-13 15:05:34 -0500

    Changed connections to cursors, removed bug in Fusion table creation

--- src/Build_Gestalt.py
+++ src/Build_Gestalt.py
@@ -7,20 +7,20 @@ tables.
 
 
 
-def create_spell_table(connection):
+def create_spell_table(cursor):
 	'''
 	Initialize the Spell table with constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Spell (
 			spell_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -41,7 +41,7 @@ def create_spell_table(connection):
 	
 
 	# this index will optimize the Accel Synchro queries in Query_Gestalt.py
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE INDEX Spell_school_casting_time_level_idx
 		ON Spell(school, casting_time, level)
@@ -51,20 +51,20 @@ def create_spell_table(connection):
 	return 0
 
 
-def create_class_table(connection):
+def create_class_table(cursor):
 	'''
 	Initialize the Class table with the PK constraint.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Class(
 			character_class_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -76,20 +76,20 @@ def create_class_table(connection):
 	return 0
 
 
-def create_fusion_table(connection):
+def create_fusion_table(cursor):
 	'''
 	Initialize the Fusion table with the PK constraint.
 
 	Paramters
 	---------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Fusion(
 			fusion_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -97,7 +97,7 @@ def create_fusion_table(connection):
 			duration TEXT NOT NULL,
 			range TEXT NOT NULL,
 			description TEXT UNIQUE NOT NULL,
-			concentration INTEGER CHECK (concentration IN (0, 1)) NOT NULL,
+			concentration INTEGER CHECK (concentration IN (0, 1)) NOT NULL
 		)
 		"""
 	)
@@ -105,20 +105,20 @@ def create_fusion_table(connection):
 	return 0
 
 
-def create_xyz_table(connection):
+def create_xyz_table(cursor):
 	'''
 	Initialize the XYZ table with constraints.
 
 	Parameters
 	----------
-	connection: sqlite3.Connection
+	cursor: sqlite3.cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE XYZ(
 			xyz_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -136,20 +136,20 @@ def create_xyz_table(connection):
 	return 0
 
 
-def create_link_table(connection):
+def create_link_table(cursor):
 	'''
 	Initialize the Link table with constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Link(
 			link_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -167,20 +167,20 @@ def create_link_table(connection):
 	return 0
 
 
-def create_spell_class_table(connection):
+def create_spell_class_table(cursor):
 	'''
 	Initialize the Spell_Class junction table with FK constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Spell_Class(
 			spell_id INTEGER NOT NULL,
@@ -195,20 +195,20 @@ def create_spell_class_table(connection):
 	return 0
 
 
-def create_spell_fusion_table(connection):
+def create_spell_fusion_table(cursor):
 	'''
 	Initialize the Spell_Fusion junction table with FK constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Spell_Fusion(
 			spell_id INTEGER NOT NULL,
@@ -221,7 +221,7 @@ def create_spell_fusion_table(connection):
 	)
 
 	# this index will optimize the get_Fusion_targets() function in Query_Gestalt.py
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE INDEX Spell_Fusion_fusion_id_idx 
 		ON Spell_Fusion(fusion_id)
@@ -231,20 +231,20 @@ def create_spell_fusion_table(connection):
 	return 0
 
 
-def create_spell_fusion_table(connection):
+def create_spell_fusion_table(cursor):
 	'''
 	Initialize the Spell_Fusion junction table with FK constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Spell_Fusion(
 			spell_id INTEGER NOT NULL,
@@ -259,20 +259,20 @@ def create_spell_fusion_table(connection):
 	return 0
 
 
-def create_xyz_class_table(connection):
+def create_xyz_class_table(cursor):
 	'''
 	Initialize the XYZ_Class junction table with FK constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE XYZ_Class(
 			xyz_id INTEGER NOT NULL,
@@ -287,20 +287,20 @@ def create_xyz_class_table(connection):
 	return 0
 
 
-def create_link_class_table(connection):
+def create_link_class_table(cursor):
 	'''
 	Initialize the Link_Class junction table with FK constraints.
 
 	Parameters
 	----------
-	connection
+	cursor
 
 	Returns
 	-------
 	0: int
 		Function successful.
 	'''
-	connection.execute(
+	cursor.execute(
 		"""
 		CREATE TABLE Link_Class(
 			link_id INTEGER NOT NULL,
@@ -311,6 +311,7 @@ def create_link_class_table(connection):
 		)
 		"""
 	)
+
 
 def main():
 
