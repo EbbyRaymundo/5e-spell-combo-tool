a1708e30231e177db415f9bbb62f028419a124c1
EbbyRaymundo <ebraymundo24@gmail.com>
2024-11-21 01:43:58 -0500

    Changed script to include working Fusion import

--- src/Populate_Gestalt.py
+++ src/Populate_Gestalt.py
@@ -8,7 +8,7 @@ def populate_main_tables(
 		connection: adbc.Connection,
 		spell_DataFrame: pl.DataFrame,
 		class_DataFrame: pl.DataFrame,
-		#fusion_DataFrame: pl.DataFrame,
+		fusion_DataFrame: pl.DataFrame,
 		xyz_DataFrame: pl.DataFrame,
 		link_DataFrame: pl.DataFrame,
 	):
@@ -27,12 +27,11 @@ def populate_main_tables(
 		connection = connection,
 		if_table_exists = "append"
 	)
-	# TODO: implement fusion_DataFrame function
-	# fusion_DataFrame.write_database(
-	# 	table_name = "Fusion",
-	# 	connection = connection,
-	# 	if_table_exists = "append"
-	# )
+	fusion_DataFrame.write_database(
+		table_name = "Fusion",
+		connection = connection,
+		if_table_exists = "append"
+	)
 	xyz_DataFrame.write_database(
 		table_name = "XYZ",
 		connection = connection,
@@ -49,7 +48,7 @@ def populate_main_tables(
 def populate_junction_tables(
 		connection: adbc.Connection,
 		spell_class_DataFrame: pl.DataFrame,
-		#spell_fusion_DataFrame: pl.DataFrame,
+		spell_fusion_DataFrame: pl.DataFrame,
 		xyz_class_DataFrame: pl.DataFrame,
 		link_class_DataFrame: pl.DataFrame
 	):
@@ -63,12 +62,11 @@ def populate_junction_tables(
 		connection = connection,
 		if_table_exists = "append"
 	)
-	# TODO: implement
-	# spell_fusion_DataFrame.write_database(
-	# 	table_name = "Spell_Fusion",
-	# 	connection = connection,
-	# 	if_table_exists = "append"
-	# )
+	spell_fusion_DataFrame.write_database(
+		table_name = "Spell_Fusion",
+		connection = connection,
+		if_table_exists = "append"
+	)
 	xyz_class_DataFrame.write_database(
 		table_name = "XYZ_Class",
 		connection = connection,
@@ -94,7 +92,7 @@ def main():
 		connection.commit()
 
 		spell_table, class_table, spell_class_table = Import_Spells.import_standard_spells("../spell_data/Spells.csv")
-		#fusion_table, spell_fusion_table = Import_Spells.import_default_fusions("../spell_data/aleisters_fusion_spells.csv")
+		fusion_table, spell_fusion_table = Import_Spells.import_default_fusions("../spell_data/aleisters_fusion_spells.csv")
 		xyz_table, xyz_class_table = Import_Spells.import_default_xyz("../spell_data/kites_xyz_spells.csv")
 		link_table, link_class_table = Import_Spells.import_default_links("../spell_data/aleisters_link_spells.csv")
 
@@ -102,21 +100,18 @@ def main():
 			connection,
 			spell_table,
 			class_table,
-			#fusion_table,
+			fusion_table,
 			xyz_table,
-			link_table,
-			
+			link_table
 		)
 
 		populate_junction_tables(
 			connection,
 			spell_class_table,
-			#spell_fusion_table,
+			spell_fusion_table,
 			xyz_class_table,
 			link_class_table
 		)
-		
-
 
 	return 0
 
