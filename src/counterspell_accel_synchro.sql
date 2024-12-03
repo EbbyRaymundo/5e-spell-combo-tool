cbc2f882c5d48e57241e061d3eb76139b7a6d8f5
EbbyRaymundo <ebraymundo24@gmail.com>
2024-12-03 13:27:43 -0500

    Update polars version requirements

--- src/counterspell_accel_synchro.sql
+++
@@ -1,5 +0,0 @@
-SELECT spell_name, level, duration, range, concentration
-FROM Spell
-WHERE (casting_time == "Action" OR casting_time == "Bonus Action")
-AND level > 5
-AND school == "Evocation";
--- src/synchro.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-cat counterspell_accel_synchro.sql | sqlite3 -column -header ../Gestalt.db
